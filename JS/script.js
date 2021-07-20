f25853d370e7050cd1b4bec717521d6ccda27797
Artsiusheuski <83862201+Artsiusheuski@users.noreply.github.com>
2021-07-20 07:53:12 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 let name = prompt ('Ваше имя?');
 let secondName = prompt ('Ваше отчество?');
 let surName = prompt ('Ваша фамилия?');
-let age = parseInt(prompt('Ваш возраст?'));
+let age = prompt('Ваш возраст?');
 let gender = confirm ('Ваш пол мужской?');
 let pension;
 
@@ -20,6 +20,6 @@ if (age >=60) {
 alert('ваше ФИО:' + ' ' + name + ' ' + secondName + ' ' + surName +
 '\nваш возраст в годах:' + ' ' + age +
 '\nваш возраст в днях:' + ' ' + ( age * 365 ) +
-'\nчерез 5 лет вам будет:' + ' ' + ( age + 5 ) +
+'\nчерез 5 лет вам будет:' + ' ' + ( +age + 5 ) +
 '\nваш пол:' + ' ' + gender +
 '\nвы на пенсии:' + ' ' + pension);
